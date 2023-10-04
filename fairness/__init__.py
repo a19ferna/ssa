f75910ec46ca812fa3efdebf3bcd2ef45b9bfaa5
agathe.fernandes <agathefernandes28@gmail.com>
2023-10-04 10:44:46 -0400

    modify name + init

--- fairness/__init__.py
+++ fairness/__init__.py
@@ -1,3 +1,4 @@
 from fairness.metrics import unfairness
 from fairness.quantile import EQF
 from fairness.wassersteinssa import Wasserstein
+from fairness.wassersteinssanobin import WassersteinNoBin
