e8b79c4f6abab969a89d65b5cb7879822c2684de
Suzie14 <suzie.grondin@gmail.com>
2023-10-11 15:59:24 -0400

    non_binary_ssa_works

--- fairness/metrics.py
+++ fairness/metrics.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def diff_quantile(data1, data2):
     """
     compute the unfairness of two populations
@@ -10,11 +11,12 @@ def diff_quantile(data1, data2):
     unfair_value = np.max(np.abs(eqf1-eqf2))
     return unfair_value
 
+
 def unfairness(y_fair, x_ssa_test):
     sens_val = list(set(x_ssa_test))
     data1 = y_fair
     lst_unfairness = []
     for mod in sens_val:
-        data2 = y_fair[x_ssa_test==mod]
+        data2 = y_fair[x_ssa_test == mod]
         lst_unfairness.append(diff_quantile(data1, data2))
-    return(lst_unfairness, max(lst_unfairness))
+    return max(lst_unfairness)
--- fairness/wassersteinssanobin.py
+++ fairness/wassersteinssanobin.py
@@ -5,62 +5,80 @@ from statsmodels.distributions.empirical_distribution import ECDF
 from fairness.quantile import EQF
 
 
-class WassersteinNoBin():
-    def __init__(self, sigma=0.0001):
-        self.sigma = sigma
-
+class BaseHelper():
+    def __init__(self):
         self.ecdf = {}
         self.eqf = {}
 
-    def fit(self, y_calib, x_ssa_calib):
-
-        if not isinstance(x_ssa_calib, np.ndarray):
+    def _check_shape(y, x_ssa):
+        if not isinstance(x_ssa, np.ndarray):
             raise ValueError('x_ssa_calib must be an array')
-        if not isinstance(y_calib, np.ndarray):
+
+        if not isinstance(y, np.ndarray):
             raise ValueError('y_calib must be an array')
 
-        if len(x_ssa_calib) != len(y_calib):
+        if len(x_ssa) != len(y):
             raise ValueError('x_ssa_calib and y should have the same length')
 
-        self.sens_val = list(set(x_ssa_calib))
-        sens_loc = {}
-        self.weights = {}
+    def _check_mod(sens_val_calib, sens_val_test):
+        if sens_val_test > sens_val_calib:
+            raise ValueError(
+                'x_ssa_test should have at most the number of modalities in x_ssa_calib')
 
-        eps = np.random.uniform(-self.sigma, self.sigma, len(y_calib))
-        for mod in self.sens_val:
-            sens_loc[mod] = np.where(x_ssa_calib == mod)[0]
-            # Fit the ecdf and eqf objects
-            self.ecdf[mod] = ECDF(y_calib[sens_loc[mod]]+eps[sens_loc[mod]])
-            self.eqf[mod] = EQF(y_calib[sens_loc[mod]]+eps[sens_loc[mod]])
+        if not all(elem in sens_val_calib for elem in sens_val_test):
+            raise ValueError(
+                'Modalities in x_ssa_test should be included in modalities of x_ssa_calib')
+
+    def _get_mod(x_ssa):
+        return list(set(x_ssa))
+
+    def _get_loc(self, x_ssa):
+        for mod in self._get_mod(x_ssa):
+            sens_loc = {}
+            sens_loc[mod] = np.where(x_ssa == mod)[0]
+            return sens_loc
+
+    def _get_weights(self, x_ssa):
+        for mod in self._get_mod(x_ssa):
             # Calculate probabilities
-            self.weights[mod] = len(sens_loc[mod])/len(x_ssa_calib)
+            weights = {}
+            weights[mod] = len(self._get_loc[mod])/len(x_ssa)
+            return weights
+
+    def _estimate_ecdf_eqf(self, y, sigma):
+        eps = np.random.uniform(-sigma, sigma, len(y))
+        for mod in self.sens_val:
+            # Fit the ecdf and eqf objects
+            self.ecdf[mod] = ECDF(y[self._get_loc[mod]] +
+                                  eps[self._get_loc[mod]])
+            self.eqf[mod] = EQF(y[self._get_loc[mod]]+eps[self._get_loc[mod]])
+
+
+class WassersteinNoBin(BaseHelper):
+    def __init__(self, sigma=0.0001):
+        BaseHelper.__init__(self)
+        self.sigma = sigma
+
+    def fit(self, y_calib, x_ssa_calib):
+        BaseHelper._check_shape(y_calib, x_ssa_calib)
+
+        self.sens_val_calib = BaseHelper._get_mod(x_ssa_calib)
+        self.weights = BaseHelper._get_weights(self, x_ssa_calib)
+        BaseHelper._estimate_ecdf_eqf(self, y_calib, self.sigma)
 
     def transform(self, y_test, x_ssa_test):
 
-        if not isinstance(x_ssa_test, np.ndarray):
-            raise ValueError('x_ssa_test must be an array')
-        if not isinstance(y_test, np.ndarray):
-            raise ValueError('y_test must be an array')
+        BaseHelper._check_shape(y_test, x_ssa_test)
+        sens_val_test = BaseHelper._get_mod(x_ssa_test)
+        BaseHelper._check_mod(self.sens_val_calib, sens_val_test)
 
-        if len(x_ssa_test) != len(y_test):
-            raise ValueError('x_ssa_test and y_test should have the same length')
-
-        if len(set(x_ssa_test)) > len(self.sens_val):
-            raise ValueError(
-                'x_ssa_test should have at most the number of modalities in x_ssa_calib')
-        
-        sens_val_test = list(set(x_ssa_test))
-        if not all(elem in self.sens_val for elem in sens_val_test):
-            raise ValueError(
-                'Modalities in x_ssa_test should be included in modalities of x_ssa_calib')
-        
-        sens_loc = {}
+        sens_loc = BaseHelper._get_loc(self, x_ssa_test)
         y_fair = np.zeros_like(y_test)
         eps = np.random.uniform(-self.sigma, self.sigma, len(y_test))
         for mod1 in sens_val_test:
-            sens_loc[mod1] = np.where(x_ssa_test == mod1)[0]
             for mod2 in sens_val_test:
                 y_fair[sens_loc[mod1]] += self.weights[mod2] * \
-                    self.eqf[mod2](self.ecdf[mod1](y_test[sens_loc[mod1]]+eps[sens_loc[mod1]]))
+                    self.eqf[mod2](self.ecdf[mod1](
+                        y_test[sens_loc[mod1]]+eps[sens_loc[mod1]]))
 
         return y_fair
