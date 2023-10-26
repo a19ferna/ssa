924c6bf629f546ab5eee3aba76135ee9d429f67b
Suzie14 <suzie.grondin@gmail.com>
2023-10-26 14:27:06 -0400

    init

--- fairness/__init__.py
+++ fairness/__init__.py
@@ -1,5 +1,3 @@
 from fairness.metrics import *
 from fairness.quantile import EQF
-from fairness.wassersteinssa import Wasserstein
-from fairness.wassersteinssanobin import WassersteinNoBin
-from fairness.approximatemultiwasserstein import MultiWasserStein
+from fairness.wasserstein import MultiWasserStein
